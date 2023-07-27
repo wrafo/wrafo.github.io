9d909e08675d463657abfaf16b39668bf0b1ca7c
Rafael Pereira <wraafo@gmail.com>
2023-07-27 00:23:10 -0300

    removed vue example page + added custom domain

---
+++ src/components/AboutMe.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="flex">
+    <div class="">
+      I'm Rafael Pereira, a passionate Software Engineer currently working in the Game Development Industry.
+      Based in Brazil, I have a deep love for creating immersive and engaging experiences through technology.
+    </div>
+    <div>
+      <img :src="img"/>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    img: String,
+  }
+}
+</script>
+
+<style scoped>
+</style>
